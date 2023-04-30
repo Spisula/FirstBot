e495421c2de435303cef451472961a4dba3b6f03
vitpr <vit.pravo@gmail.com>
2023-04-30 14:00:59 +1000

    training

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import time
 
 
 API_URL: str = 'https://api.telegram.org/bot'
-BOT_TOKEN: str = '5424991242:AAGwomxQz1p46bRi_2m3V7kvJlt5RjK9xr0'
-TEXT: str = 'Ура! Классный апдейт!'
+BOT_TOKEN: str = '5886220274:AAH6QAYckGGLKQAx22bQ4Jb2tUYLMS_a2sU'
+TEXT: str = 'Классный апдейт!'
 MAX_COUNTER: int = 100
 
 offset: int = -2
@@ -17,12 +17,15 @@ while counter < MAX_COUNTER:
     print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
 
     updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    print(type(updates))
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
             chat_id = result['message']['from']['id']
-            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+            user_name = result['message']['from']['first_name']
+            user_mess = result['message']['text']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={user_name}, "{user_mess}" is good message')
 
     time.sleep(1)
     counter += 1
