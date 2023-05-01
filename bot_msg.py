a9b4d8ef21cdcd33f1cd74185b41688494158727
vitpr <vit.pravo@gmail.com>
2023-05-01 21:10:30 +1000

    обработка разных типов сообщений

---
+++ bot_msg.py
@@ -0,0 +1,12 @@
+random_stickers = {1:'CAACAgIAAxkBAAIBsGRPfevCf87UOSqhQN32GVryerIJAAKQEAACa16oSBwaTsyegEWYLwQ',
+                   2:'CAACAgIAAxkBAAIBsmRPfmUOO_fITHL20uQrHqdF0w4GAAIQAAPANk8T6oGKKfEfAugvBA',
+                   3:'CAACAgIAAxkBAAIBtGRPfn4ckTudthWmT1Cm4RrY2RzXAAIVAAPANk8TzVamO2GeZOcvBA',
+                   4:'CAACAgIAAxkBAAIBtmRPfs7SeU0-O09GDl-QtZkuFFkHAAIDAAPANk8TpCnu5n3zwHgvBA',
+                   5:'CAACAgIAAxkBAAIBuGRPfvXNotS9KAf1zsY2i2-8gzQPAAJCEAACM8UpSZAO1BGnKkqCLwQ',
+                   6:'CAACAgIAAxkBAAIBumRPfxWPewES54OYIzMxwloKtM-WAAIUAAPANk8TrWWZ5Lkw9j4vBA',
+                   7:'CAACAgIAAxkBAAIB02RPgdzCRSwzRmVJBtOJuAgyZUA1AAIYAAPANk8T1vonv5xqGPgvBA',
+                   8:'CAACAgIAAxkBAAIB22RPglY5656l46NL_rV2wJYNsWFLAAJaAAPANk8TC_wPT9xGGeEvBA'
+                   }
+
+bot_text = {1:'Привет!', 2:'Попробуй еще раз', 3:'Я тебя не понимаю', 4:'Рад тебя видеть!',
+            5:'Как тебя зовут?'}
--- main.py
+++ main.py
@@ -1,27 +1,50 @@
-import requests
-import time
-from settings import BOT_TOKEN
+from aiogram import Bot, Dispatcher, F
+from aiogram.filters import Command
+from aiogram.types import Message
+from config import BOT_TOKEN
+from random import randint, choice
+import bot_msg
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=BOT_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+# Этот хэндлер будет срабатывать на команду "/start"
+async def start_command(message: Message):
+    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+
+async def help_command(msg: Message):
+    await msg.answer('Сейчас я не могу помочь, так как раздел не создан')
+
+async def send_file(msg: Message):
+    await msg.answer('Кнопка находится в стадии разработки')
 
 
-API_URL: str = 'https://api.telegram.org/bot'
-offset: int = -2
-updates: dict
-timeout = 60
+# Этот хэндлер будет срабатывать на отправку боту фото
+async def send_photo_echo(message: Message):
+    await message.reply_photo(message.photo[0].file_id)
 
-def do_something() -> None:
-    print('Был апдейт')
+async def sticker_echo(msg: Message):
+    print(msg.sticker.file_id)
+    await msg.answer_sticker(sticker = bot_msg.random_stickers[randint(1, 8)])
 
 
-while True:
-    start_time = time.time()
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}&timeout={timeout}').json()
+# Этот хэндлер будет срабатывать на отправку стикеров
+#async def send_sticker_echo(message: Message):
+ #   await message.answer_sticker(message.sticker.file_id)
 
-    if updates['result']:
-        for result in updates['result']:
-            print(result['message']['text'])
-            offset = result['update_id']
-            do_something()
+#@dp.message()
+async def all_echo(msg: Message):
+    name = msg.from_user.first_name
+    await msg.answer(f'{name}, {bot_msg.bot_text[(randint(1, 5))]}')
 
-    time.sleep(1)
-    end_time = time.time()
-    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')
+# Регистрируем хэндлеры
+dp.message.register(start_command, Command(commands=["start"]))
+dp.message.register(help_command, Command('help'))
+dp.message.register(send_file, Command('sendfile'))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(sticker_echo, F.sticker)
+dp.message.register(all_echo)
+
+dp.run_polling(bot)
+
